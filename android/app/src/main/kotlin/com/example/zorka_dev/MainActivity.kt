8fd1f9b66f747e31c4082c519308436e4a908bd4
Priteshverma123 <Pritesh_verma>
2024-03-23 22:19:35 +0530

    START

---
+++ android/app/src/main/kotlin/com/example/zorka_dev/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.zorka_dev
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:zorka_dev/navbar/navbar.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.deepPurple,
+        ).copyWith(
+          secondary: Colors.white,
+        ),
+      ),
+      home: const NavigationMenu(),
+    );
+  }
+}
